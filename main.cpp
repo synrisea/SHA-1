1272fb90f56273f05264a2e27cb85f83ec224f98
synrisea <150649087+synrisea@users.noreply.github.com>
2023-12-18 21:56:32 +0400

    Introducing non-linearity
     Logical function applied to process the input data in block of 512 bits.

--- main.cpp
+++ main.cpp
@@ -9,6 +9,30 @@ T leftCycleShift(T value, int positions)
     return (value << positions) | (value >> (bit - positions));
 }
 
+unsigned int nonLinearity(int i, unsigned int B, unsigned int C, unsigned int D)
+{
+    unsigned int result;
+
+    if (0 <= i && i <= 19)
+    {
+        result = ( B & C ) | ( ( ~B ) & D );
+    }
+    else if (20 <= i && i <= 39) 
+    {
+        result = B ^ C ^ D;
+    }
+    else if (40 <= i && i <= 59) 
+    {
+        result = ( B & C ) | ( B & D ) | ( C & D );
+    }
+    else if (60 <= i && i <= 79) 
+    {
+        result = B ^ C ^ D;
+    }
+
+    return result;
+}
+
 int main()
 {
     return 0;
